3a28419f51b2207b551cc25b3f0563c4b13b4589
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-10-09 17:11:10 -0300

    ...

---
+++ sql/00-CREATE_DATABASE.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE `crosier_riotur_dev` CHARACTER SET 'utf8' COLLATE 'utf8_swedish_ci';
+GRANT ALL PRIVILEGES ON crosier_riotur_dev.* TO 'crosier_riotur_dev'@'localhost' IDENTIFIED BY 'crosier_riotur_dev';
+FLUSH PRIVILEGES;
